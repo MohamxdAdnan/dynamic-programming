af4848b54af2fd8c2bf6b449bc4cd8c936686779
Mohamed Adnan <mohamxd.adnan@gmail.com>
2024-07-19 10:02:13 +0530

    largest-sum

--- LargestSum.java
+++ LargestSum.java
@@ -1,5 +1,17 @@
 public class LargestSum {
     public static void main(String[] args) {
+        int arr[]={5, 4, -1, 7, 8};
+        int largestSum=0;
 
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum=arr[i];
+            for (int k = i+1; k < arr.length; k++) {
+                sum=sum+arr[k];
+                if(sum>largestSum)
+                    largestSum=sum;
+            }
+        }
+        System.out.println(largestSum);
     }
 }
